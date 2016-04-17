92bf51fc45bd56c91521ef8c854a78b0eed07b6f
{심현수} <{tlagustn88@naver.com}>
2016-04-17 15:55:22 +0900

    a

--- web/js/simpolio.js
+++ web/js/simpolio.js
@@ -32,7 +32,6 @@ var SimPolio = (function(){
     
     SimPolio.prototype.set = function(wNum){ //wNum = 한줄당 들어갈수잇는 포트폴리오 갯수
         this.liLenght = this.simpolio.find('.s_thum_view').length;
-        //console.log(this.liLenght);
         this.simpolio.find('.s_thum').css({
             'width' : 100/this.viewNum+'%'
         })
@@ -48,20 +47,15 @@ var SimPolio = (function(){
 		
         this.set(this.viewNum);
 
-		console.log('한줄갯수',this.viewNum);
         this.portW = this.simpolio.find('.s_thum').innerWidth();// 가로:세로 1:1 비율이여서 높이값에 대신 Width 값 사용됨.
         //this.portH = this.simpolio.find('.s_thum').innerHeight();
-        //console.log('높이',this.portH );
         
         this.portLine = Math.ceil(this.liLenght/this.viewNum);
-        console.log('라인수',':',this.portLine);
         
         var portLineH = this.simpolio.find('.s_thum').innerWidth();
-        //console.log(this.portTop);
         for(var n=1; n <= this.portLine; n++){
             this.portLeft = 0 
             this.portTop += portLineH;
-            console.log(portLineH);
             for(var i=0; i < this.viewNum; i++){
                 this.simpolio.children('.s_thum:eq('+(this.viewNum*(n-1)+i)+')').animate({
                     'top' : this.portTop-portLineH,
@@ -72,10 +66,6 @@ var SimPolio = (function(){
 
 		var aa = this.simpolio.find('img').height();
         this.viewHeight = this.portW*this.portLine;
-        //console.log('썸높이',$('.s_thum').height());
-        //console.log('이미지높이',$('.s_thum_view img'));
-        //console.log('이미지높이',$('.s_thum_view img').height());
-		//console.log($('.portfolio_view').width());
         this.simpolio.closest('.portfolio_view').css({'height' : this.viewHeight});
 	}
     
@@ -87,7 +77,6 @@ var SimPolio = (function(){
 			_this.set(_this.viewNum);
             var resizePortLeft = 0;
             _this.resizeProtW = _this.simpolio.find('.s_thum').innerWidth();
-            console.log(_this.resizeProtW);
 			_this.resizePortLine = Math.ceil(_this.liLenght/_this.viewNum);
             _this.portLineH = _this.simpolio.find('.s_thum').innerHeight();
             var resizePortTop = 0;
@@ -102,8 +91,6 @@ var SimPolio = (function(){
                 }
             }
             _this.resizeViewHeight = _this.portLineH*_this.resizePortLine;
-			console.log('라인',_this.resizePortLine);
-            console.log('리사이즈높이',_this.resizeViewHeight);
 			_this.simpolio.closest('.portfolio_view').stop().animate({'height' : _this.resizeViewHeight},300);
         })
     }
